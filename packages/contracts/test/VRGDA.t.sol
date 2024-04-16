cca867ca1252522b97436077888c2d2b9e6f4931
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-04-16 16:45:55 -0400

    init

---
+++ packages/contracts/test/VRGDA.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.22;
+
+import { Test } from "forge-std/Test.sol";
+
+contract VrgdaTest is Test {
+    function setUp() public virtual {}
+}
